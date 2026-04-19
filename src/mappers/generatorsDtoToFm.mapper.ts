edf8ae10defc168aec05f46d127ab707e126a8b7
2Nice <156375468+jst2Nice@users.noreply.github.com>
2026-04-19 16:49:37 +0200

    Update generatorsDtoToFm.mapper.ts

--- src/mappers/generatorsDtoToFm.mapper.ts
+++ src/mappers/generatorsDtoToFm.mapper.ts
@@ -3,7 +3,7 @@ import type { GeneratorDto } from "../types/apis/dataTransferObject/generatorsDt
 import type { GeothermalGeneratorDto } from "../types/apis/dataTransferObject/geothermalGeneratorDto";
 import type { GeneratorFm } from "../types/apis/frontModel/generatorsFm";
 import { enumDtoToFmMapper } from "./enumDtoToFm.mapper";
-
+import { v4 as uuidv4 } from 'uuid';
 
 export const generatorsDtoToFmMapper = (
   dto: GeneratorDto[] | GeothermalGeneratorDto[],
